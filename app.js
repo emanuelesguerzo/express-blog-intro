d86bd863a1001543cdc9194b9ec96d79e4b60d36
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-12-02 14:14:28 +0100

    feat: added another route and static assets

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require('express');
+const express = require("express");
+const data = require("./data.js")
 const app = express();
 const port = 3001;
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    res.send('Server del mio Blog');
+app.get("/", (req, res) => {
+    res.send("Server del mio Blog");
 });
+
+app.get("/bacheca", (req, res) => {
+    const posts = {
+        post: data.posts,
+        lunghezza: data.posts.length,
+    }
+    res.json(posts);
+})
 
 app.listen(port, () => {
     console.log(`Server in esecuzione sulla porta ${port}`);
---
+++ data.js
@@ -0,0 +1,36 @@
+const posts = [
+    {
+        title: "Ciambellone",
+        content: "Un dolce soffice e profumato, perfetto per la colazione o la merenda.",
+        image: "/img/ciambellone.jpeg",
+        tags: ["dolce", "colazione", "tradizionale"]
+    },
+    {
+        title: "Cracker alla barbabietola",
+        content: "Sfiziosi cracker dal colore vivace e dal sapore unico grazie alla barbabietola.",
+        image: "/img/cracker_barbabietola.jpeg",
+        tags: ["snack", "salato", "vegetariano"]
+    },
+    {
+        title: "Pane fritto dolce",
+        content: "Una golosa ricetta tradizionale con pane fritto e zucchero, ideale per un dessert semplice.",
+        image: "/img/pane_fritto_dolce.jpeg",
+        tags: ["dolce", "fritto", "semplice"]
+    },
+    {
+        title: "Pasta alla barbabietola",
+        content: "Un primo piatto originale e colorato con il gusto delicato della barbabietola.",
+        image: "/img/pasta_barbabietola.jpeg",
+        tags: ["primo", "vegetariano", "colorato"]
+    },
+    {
+        title: "Torta paesana",
+        content: "Un dolce rustico della tradizione, preparato con ingredienti semplici e genuini.",
+        image: "/img/torta_paesana.jpeg",
+        tags: ["dolce", "tradizionale", "rustico"]
+    }
+];
+
+module.exports = {
+    posts,
+}
